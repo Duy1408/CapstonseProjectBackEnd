33a8bdea8299f3178814780fe8934112e97e1adf
Duy1408 <duynguyen08142001@gmail.com>
2024-07-04 16:14:31 +0700

    update-db

---
+++ RealEstateProjectSaleBusinessObject/Migrations/20240704091307_v4.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstateProjectSaleBusinessObject.Migrations
+{
+    public partial class v4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Project",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Project");
+        }
+    }
+}
