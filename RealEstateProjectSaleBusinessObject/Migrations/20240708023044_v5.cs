26c6ae26d462564597b006cca9fe05d5e5a84937
Duy1408 <duynguyen08142001@gmail.com>
2024-07-08 09:34:04 +0700

    Update-database

---
+++ RealEstateProjectSaleBusinessObject/Migrations/20240708023044_v5.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstateProjectSaleBusinessObject.Migrations
+{
+    public partial class v5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PropertiesID",
+                table: "Comment");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PropertyID",
+                table: "Comment",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PropertyID",
+                table: "Comment",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "PropertiesID",
+                table: "Comment",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
