868a96d0851dcb380c4ea711d2e328852c8224d5
duongmake <duongmake3@gmail.com>
2024-07-08 11:04:26 +0700

    commit Update Customer

--- RealEstateProjectSaleBusinessObject/DTO/Update/CustomerUpdateDTO.cs
+++ RealEstateProjectSaleBusinessObject/DTO/Update/CustomerUpdateDTO.cs
@@ -9,21 +9,17 @@ namespace RealEstateProjectSaleBusinessObject.DTO.Update
 {
     public class CustomerUpdateDTO
     {
-        [JsonIgnore]
-        public Guid CustomerID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public string PersonalEmail { get; set; }
-        public string PhoneNumber { get; set; }
-        public string IdentityCardNumber { get; set; }
-        public string Nationality { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? PersonalEmail { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? IdentityCardNumber { get; set; }
+        public string? Nationality { get; set; }
         public string? Taxcode { get; set; }
         public string? BankName { get; set; }
         public int? BankNumber { get; set; }
-        public string Address { get; set; }
-        public bool Status { get; set; }
-        [JsonIgnore]
-        public Guid AccountID { get; set; }
+        public string? Address { get; set; }
+        public bool? Status { get; set; }
     }
 }
