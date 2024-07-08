82adcdb14da7f903dca3a17916b69d0f8e60b104
Duy1408 <duynguyen08142001@gmail.com>
2024-07-08 21:28:54 +0700

    map-project

---
+++ RealEstateProjectSaleBusinessObject/DTO/Create/ProjectCreateDTO.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateProjectSaleBusinessObject.DTO.Create
+{
+    public class ProjectCreateDTO
+    {
+        public string ProjectName { get; set; }
+        public string CommericalName { get; set; }
+        public string ShortName { get; set; }
+        public string TypeOfProject { get; set; }
+        public string Address { get; set; }
+        public string Commune { get; set; }
+        public string District { get; set; }
+        public double DepositPrice { get; set; }
+        public string? Summary { get; set; }
+        public int? LicenseNo { get; set; }
+        public DateTime? DateOfIssue { get; set; }
+        public string? CampusArea { get; set; }
+        public string? PlaceofIssue { get; set; }
+        public string? Code { get; set; }
+        public IFormFile? Image { get; set; }
+       
+    }
+}
