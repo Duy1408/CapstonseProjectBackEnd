6da2848a92e83738b0e9279a988cc2e93260e78d
Duy1408 <duynguyen08142001@gmail.com>
2024-07-09 14:46:12 +0700

    update-project

--- RealEstateProjectSaleBusinessObject/DTO/Update/ProjectUpdateDTO.cs
+++ RealEstateProjectSaleBusinessObject/DTO/Update/ProjectUpdateDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,7 +22,7 @@ namespace RealEstateProjectSaleBusinessObject.DTO.Update
         public string? CampusArea { get; set; }
         public string? PlaceofIssue { get; set; }
         public string? Code { get; set; }
-        public string? Image { get; set; }
+        public IFormFile? Image { get; set; }
         public string? Status { get; set; }
     }
 }
