ae412c9c549db3178247207d5623f8b251d382d8
duongmake <duongmake3@gmail.com>
2024-08-01 08:32:09 +0700

    commit Stripe Payment 1st

---
+++ RealEstateProjectSaleBusinessObject/Model/PaymentResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateProjectSaleBusinessObject.Model
+{
+    public class PaymentResponseModel
+    {
+        public string? SessionId { get; set; }
+
+        public string? PubKey { get; set; }
+    }
+}
