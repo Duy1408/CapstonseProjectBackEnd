6970043e00bee960501f494957494079f4ca445e
duongmake <duongmake3@gmail.com>
2024-10-05 12:47:54 +0700

    commit Fix Booking V1

--- RealEstateProjectSaleBusinessObject/DTO/Update/BookingUpdateDTO.cs
+++ RealEstateProjectSaleBusinessObject/DTO/Update/BookingUpdateDTO.cs
@@ -12,7 +12,11 @@ namespace RealEstateProjectSaleBusinessObject.DTO.Update
         public IFormFile? BookingFile { get; set; }
         public string? Note { get; set; }
         public string? Status { get; set; }
-        public Guid? PropertyID { get; set; }
+        public Guid CustomerID { get; set; }
         public Guid? StaffID { get; set; }
+        public Guid OpenForSaleID { get; set; }
+        public Guid PropertyCategoryID { get; set; }
+        public Guid? DocumentID { get; set; }
+
     }
 }
