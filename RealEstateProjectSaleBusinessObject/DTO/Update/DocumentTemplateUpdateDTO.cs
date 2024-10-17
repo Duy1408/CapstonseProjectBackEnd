851307ce5d7e18b7baf1847af3fd76111962e74b
Duy1408 <duynguyen08142001@gmail.com>
2024-10-17 11:03:34 +0700

    fix-document

--- RealEstateProjectSaleBusinessObject/DTO/Update/DocumentTemplateUpdateDTO.cs
+++ RealEstateProjectSaleBusinessObject/DTO/Update/DocumentTemplateUpdateDTO.cs
@@ -10,7 +10,7 @@ namespace RealEstateProjectSaleBusinessObject.DTO.Update
     public class DocumentTemplateUpdateDTO
     {
         public string? DocumentName { get; set; }
-        public IFormFile? DocumentFile { get; set; }
+        public string? DocumentFile { get; set; }
         public bool? Status { get; set; }
     }
 }
