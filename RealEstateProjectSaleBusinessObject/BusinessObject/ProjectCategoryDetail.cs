ecca5dc5ff83a94a32b93fc600cfadc771c41a5e
Duy1408 <duynguyen08142001@gmail.com>
2024-10-31 21:20:56 +0700

    update-db

--- RealEstateProjectSaleBusinessObject/BusinessObject/ProjectCategoryDetail.cs
+++ RealEstateProjectSaleBusinessObject/BusinessObject/ProjectCategoryDetail.cs
@@ -8,11 +8,12 @@ namespace RealEstateProjectSaleBusinessObject.BusinessObject
 {
     public class ProjectCategoryDetail
     {
+        public Guid ProjectCategoryDetailID { get; set; }
         public Guid ProjectID { get; set; }
         public Project? Project { get; set; }
         public Guid PropertyCategoryID { get; set; }
         public PropertyCategory? PropertyCategory { get; set; }
-        public List<Property> Properties { get; set; }
+       public List<Property> Properties { get; set; }
 
     }
 }
---
+++ RealEstateProjectSaleBusinessObject/Migrations/20241031141816_v33.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstateProjectSaleBusinessObject.Migrations
+{
+    public partial class v33 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Property_ProjectCategoryDetail_ProjectCategoryDetailProjectID_ProjectCategoryDetailPropertyCategoryID",
+                table: "Property");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Property_ProjectCategoryDetailProjectID_ProjectCategoryDetailPropertyCategoryID",
+                table: "Property");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProjectCategoryDetail",
+                table: "ProjectCategoryDetail");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectCategoryDetailProjectID",
+                table: "Property");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectCategoryDetailPropertyCategoryID",
+                table: "Property");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectID",
+                table: "Property");
+
+            migrationBuilder.RenameColumn(
+                name: "PropertyCategoryID",
+                table: "Property",
+                newName: "ProjectCategoryDetailID");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProjectCategoryDetailID",
+                table: "ProjectCategoryDetail",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProjectCategoryDetail",
+                table: "ProjectCategoryDetail",
+                column: "ProjectCategoryDetailID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Property_ProjectCategoryDetailID",
+                table: "Property",
+                column: "ProjectCategoryDetailID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectCategoryDetail_ProjectID",
+                table: "ProjectCategoryDetail",
+                column: "ProjectID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Property_ProjectCategoryDetail_ProjectCategoryDetailID",
+                table: "Property",
+                column: "ProjectCategoryDetailID",
+                principalTable: "ProjectCategoryDetail",
+                principalColumn: "ProjectCategoryDetailID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Property_ProjectCategoryDetail_ProjectCategoryDetailID",
+                table: "Property");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Property_ProjectCategoryDetailID",
+                table: "Property");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProjectCategoryDetail",
+                table: "ProjectCategoryDetail");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProjectCategoryDetail_ProjectID",
+                table: "ProjectCategoryDetail");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectCategoryDetailID",
+                table: "ProjectCategoryDetail");
+
+            migrationBuilder.RenameColumn(
+                name: "ProjectCategoryDetailID",
+                table: "Property",
+                newName: "PropertyCategoryID");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProjectCategoryDetailProjectID",
+                table: "Property",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProjectCategoryDetailPropertyCategoryID",
+                table: "Property",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProjectID",
+                table: "Property",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProjectCategoryDetail",
+                table: "ProjectCategoryDetail",
+                columns: new[] { "ProjectID", "PropertyCategoryID" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Property_ProjectCategoryDetailProjectID_ProjectCategoryDetailPropertyCategoryID",
+                table: "Property",
+                columns: new[] { "ProjectCategoryDetailProjectID", "ProjectCategoryDetailPropertyCategoryID" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Property_ProjectCategoryDetail_ProjectCategoryDetailProjectID_ProjectCategoryDetailPropertyCategoryID",
+                table: "Property",
+                columns: new[] { "ProjectCategoryDetailProjectID", "ProjectCategoryDetailPropertyCategoryID" },
+                principalTable: "ProjectCategoryDetail",
+                principalColumns: new[] { "ProjectID", "PropertyCategoryID" });
+        }
+    }
+}
