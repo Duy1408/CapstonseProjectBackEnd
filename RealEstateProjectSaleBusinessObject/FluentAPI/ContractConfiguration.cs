bde4ef183279f729909d26fe3cb2d8e6c9fb42a5
Duy1408 <duynguyen08142001@gmail.com>
2024-11-05 09:26:08 +0700

    add-column

--- RealEstateProjectSaleBusinessObject/FluentAPI/ContractConfiguration.cs
+++ RealEstateProjectSaleBusinessObject/FluentAPI/ContractConfiguration.cs
@@ -26,6 +26,7 @@ namespace RealEstateProjectSaleBusinessObject.FluentAPI
             builder.Property(x => x.Description);
             builder.Property(x => x.ContractDepositFile);
             builder.Property(x => x.ContractSaleFile);
+            builder.Property(x => x.PriceSheetFile);
             builder.Property(x => x.Status).IsRequired();
             builder.HasOne(x => x.Booking);
             builder.HasMany(x => x.ContractPaymentDetails).WithOne(x => x.Contract);
