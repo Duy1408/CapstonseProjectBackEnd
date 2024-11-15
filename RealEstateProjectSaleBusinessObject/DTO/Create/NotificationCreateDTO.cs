6cc8cae919a851be60410a6d50289b2a80d3cdd8
duongmake <duongmake3@gmail.com>
2024-11-15 11:06:41 +0700

    commit Fix Notification V2

--- RealEstateProjectSaleBusinessObject/DTO/Create/NotificationCreateDTO.cs
+++ RealEstateProjectSaleBusinessObject/DTO/Create/NotificationCreateDTO.cs
@@ -15,7 +15,6 @@ namespace RealEstateProjectSaleBusinessObject.DTO.Create
         public string Subtiltle { get; set; }
         public string Body { get; set; }
         public string DeepLink { get; set; }
-        public Guid CustomerID { get; set; }
-        public Guid OpeningForSaleID { get; set; }
+        public Guid BookingID { get; set; }
     }
 }
