50fe7d6064f1d2e5e7f7e7c4f6caeb13151e2e12
duongmake <duongmake3@gmail.com>
2024-11-17 12:53:29 +0700

    commit Fix Contract Post

--- RealEstateProjectSaleBusinessObject/DTO/Request/ContractRequestDTO.cs
+++ RealEstateProjectSaleBusinessObject/DTO/Request/ContractRequestDTO.cs
@@ -11,18 +11,10 @@ namespace RealEstateProjectSaleBusinessObject.DTO.Request
 {
     public class ContractRequestDTO
     {
-        public string ContractCode { get; set; }
-        public string ContractType { get; set; }
-        [Column(TypeName = "date")]
-        public DateTime? ExpiredTime { get; set; }
         public double? TotalPrice { get; set; }
         public string? Description { get; set; }
         public IFormFile? ContractDepositFile { get; set; }
-        public Guid? DocumentTemplateID { get; set; }
         public Guid BookingID { get; set; }
-        public Guid CustomerID { get; set; }
-        public Guid? PaymentProcessID { get; set; }
-        public Guid? PromotionDetailID { get; set; }
 
     }
 }
