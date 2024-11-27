607e76ca1e611c8cd5b538661cffe876a35a4608
duongmake <duongmake3@gmail.com>
2024-11-27 21:03:12 +0700

    commit Update Notification Validator

---
+++ RealEstateProjectSale/Validations/Create/NotificationCreateDTOValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using RealEstateProjectSaleBusinessObject.DTO.Create;
+
+namespace RealEstateProjectSale.Validations.Create
+{
+    public class NotificationCreateDTOValidator : AbstractValidator<NotificationCreateDTO>
+    {
+        public NotificationCreateDTOValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Tiêu đề không được để trống.")
+                .MaximumLength(200).WithMessage("Tiêu đề không được dài quá 200 ký tự.");
+
+            RuleFor(x => x.Subtiltle)
+                .NotEmpty().WithMessage("Phụ đề không được để trống.")
+                .MaximumLength(200).WithMessage("Phụ đề không được dài quá 200 ký tự.");
+
+            RuleFor(x => x.Body)
+                .NotEmpty().WithMessage("Nội dung không được để trống.")
+                .MaximumLength(1000).WithMessage("Nội dung không được dài quá 1000 ký tự.");
+
+        }
+    }
+}
---
+++ RealEstateProjectSale/Validations/Update/NotificationUpdateDTOValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using RealEstateProjectSaleBusinessObject.DTO.Create;
+using RealEstateProjectSaleBusinessObject.DTO.Update;
+
+namespace RealEstateProjectSale.Validations.Update
+{
+    public class NotificationUpdateDTOValidator : AbstractValidator<NotificationUpdateDTO>
+    {
+        public NotificationUpdateDTOValidator()
+        {
+            RuleFor(x => x.Title)
+                .MaximumLength(200).WithMessage("Tiêu đề không được dài quá 200 ký tự.")
+                .When(x => !string.IsNullOrEmpty(x.Title));
+
+            RuleFor(x => x.Subtiltle)
+                .MaximumLength(200).WithMessage("Phụ đề không được dài quá 200 ký tự.")
+                .When(x => !string.IsNullOrEmpty(x.Subtiltle));
+
+            RuleFor(x => x.Body)
+                .MaximumLength(1000).WithMessage("Nội dung không được dài quá 1000 ký tự.")
+                .When(x => !string.IsNullOrEmpty(x.Body));
+
+            RuleFor(x => x.Status)
+                .Must(status => status == null || status == true || status == false).WithMessage("Trạng thái không hợp lệ nếu có.");
+
+        }
+    }
+}
