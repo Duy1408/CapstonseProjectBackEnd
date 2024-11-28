685f3303e15f7b819cff9f2ebf32cbafdd7520fa
duongmake <duongmake3@gmail.com>
2024-11-28 11:10:37 +0700

    commit Update Contract

--- RealEstateProjectSale/SwaggerResponses/ContractResponse.cs
+++ RealEstateProjectSale/SwaggerResponses/ContractResponse.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using RealEstateProjectSaleBusinessObject.JsonConverters;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RealEstateProjectSale.SwaggerResponses
 {
@@ -9,6 +10,7 @@ namespace RealEstateProjectSale.SwaggerResponses
         public string PropertyCode { get; set; }
         public double? PriceSold { get; set; }
         [Column(TypeName = "date")]
+        [JsonConverter(typeof(DateOnlyConverter))]
         public DateTime? ExpiredTime { get; set; }
         public string Status { get; set; }
     }
