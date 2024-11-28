9d21190fe980ecb774c91bbe2b0fd25db4a00534
duongmake <duongmake3@gmail.com>
2024-11-28 09:36:49 +0700

    commit Update Sale Policy

--- RealEstateProjectSaleBusinessObject/DTO/Create/SalepolicyCreateDTO.cs
+++ RealEstateProjectSaleBusinessObject/DTO/Create/SalepolicyCreateDTO.cs
@@ -4,17 +4,20 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace RealEstateProjectSaleBusinessObject.DTO.Create
 {
     public class SalepolicyCreateDTO
     {
+        [JsonIgnore]
         public Guid SalesPolicyID { get; set; }
         public string SalesPolicyType { get; set; }
         [Column(TypeName = "date")]
         public DateTime ExpressTime { get; set; }
         public string? PeopleApplied { get; set; }
+        [JsonIgnore]
         public bool Status { get; set; }
         public Guid ProjectID { get; set; }
 
