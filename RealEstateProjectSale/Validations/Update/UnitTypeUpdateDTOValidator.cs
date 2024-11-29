f739431ebe76870fa9253d2b0d575835e902d4ea
Duy1408 <duynguyen08142001@gmail.com>
2024-11-29 10:13:47 +0700

    fix-uniittypevalidator

--- RealEstateProjectSale/Validations/Update/UnitTypeUpdateDTOValidator.cs
+++ RealEstateProjectSale/Validations/Update/UnitTypeUpdateDTOValidator.cs
@@ -10,31 +10,33 @@ namespace RealEstateProjectSale.Validations.Update
 
             RuleFor(x => x.BathRoom)
               .NotEmpty().WithMessage(" Nhập số phòng tắm ")
-              .InclusiveBetween(1, 5).WithMessage("Số phòng tắm phải từ 1 đến 5.");
+              .InclusiveBetween(1, 5).WithMessage("Số phòng tắm phải từ 1 đến 5.")
+              .When(x => x.BathRoom.HasValue);
 
             RuleFor(x => x.NetFloorArea)
                .Must(x => x > 10).WithMessage("Diện tích sàn phải lớn hơn 10m².")
-               .GreaterThan(10).WithMessage("Diện tích sàn phải lớn hơn 10.")
                .LessThanOrEqualTo(400).WithMessage("Diện tích sàn không được vượt quá 400m².");
 
             RuleFor(x => x.GrossFloorArea)
-               .Must(x => x > 10).WithMessage(" Tổng diện tích sàn phải lớn hơn 10m².")
-               .GreaterThan(10).WithMessage("Tổng diện tích phải lớn hơn 10m².")
+               .Must(x => x > 10).WithMessage(" Tổng diện tích phải lớn hơn 10m².")
                .LessThanOrEqualTo(400).WithMessage("Tổng diện tích không được vượt quá 400m².")
                .Must((model, grossFloorArea) => grossFloorArea > model.NetFloorArea)
                .WithMessage("Tổng diện tích phải lớn hơn diện tích sàn.");
 
             RuleFor(x => x.BedRoom)
               .NotEmpty().WithMessage(" Nhập số phòng ngủ ")
-              .InclusiveBetween(1, 5).WithMessage("Số phòng ngủ phải từ 1 đến 5.");
+              .InclusiveBetween(1, 5).WithMessage("Số phòng ngủ phải từ 1 đến 5.")
+              .When(x => x.BedRoom.HasValue);
 
             RuleFor(x => x.KitchenRoom)
               .NotEmpty().WithMessage(" Nhập số nhà bếp ")
-              .InclusiveBetween(1, 3).WithMessage("Số nhà bếp phải từ 1 đến 3.");
+              .InclusiveBetween(1, 3).WithMessage("Số nhà bếp phải từ 1 đến 3.")
+              .When(x => x.KitchenRoom.HasValue);
 
             RuleFor(x => x.LivingRoom)
               .NotEmpty().WithMessage(" Nhập số phòng khách ")
-              .InclusiveBetween(1, 3).WithMessage("Số phòng khách phải từ 1 đến 3.");
+              .InclusiveBetween(1, 3).WithMessage("Số phòng khách phải từ 1 đến 3.")
+              .When(x => x.LivingRoom.HasValue);
 
             RuleFor(x => x.NumberFloor)
               .InclusiveBetween(1, 4).WithMessage("Số tầng phải từ 1 đến 4.");
