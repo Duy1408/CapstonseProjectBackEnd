434fca4bcc14716cd440ac736582e6ea530ac8e7
Duy1408 <duynguyen08142001@gmail.com>
2024-11-29 09:59:37 +0700

    fix-validation-uniittpe

--- RealEstateProjectSale/Validations/Request/UnitTypeRequestDTOValidator.cs
+++ RealEstateProjectSale/Validations/Request/UnitTypeRequestDTOValidator.cs
@@ -12,13 +12,11 @@ namespace RealEstateProjectSale.Validations.Request
                .InclusiveBetween(1, 5).WithMessage("Số phòng tắm phải từ 1 đến 5.");
 
             RuleFor(x => x.NetFloorArea)
-               .NotEmpty().WithMessage("Nhập diện tích sàn.")
                .Must(x => x > 10).WithMessage("Diện tích sàn phải lớn hơn 10m².")
                .GreaterThan(10).WithMessage("Diện tích sàn phải lớn hơn 10.")
                .LessThanOrEqualTo(400).WithMessage("Diện tích sàn không được vượt quá 400m².");
 
             RuleFor(x => x.GrossFloorArea)
-               .NotEmpty().WithMessage("Nhập tổng diện tích.")
                .Must(x => x > 10).WithMessage(" Tổng diện tích sàn phải lớn hơn 10m².")
                .GreaterThan(10).WithMessage("Tổng diện tích phải lớn hơn 10m².")
                .LessThanOrEqualTo(400).WithMessage("Tổng diện tích không được vượt quá 400m².")
@@ -38,11 +36,9 @@ namespace RealEstateProjectSale.Validations.Request
               .InclusiveBetween(1, 3).WithMessage("Số phòng khách phải từ 1 đến 3.");
 
             RuleFor(x => x.NumberFloor)
-              .NotEmpty().WithMessage(" Nhập số tầng ")
               .InclusiveBetween(1, 4).WithMessage("Số tầng phải từ 1 đến 4.");
 
             RuleFor(x => x.Basement)
-             .NotEmpty().WithMessage(" Nhập số tầng hầm ")
              .InclusiveBetween(1, 3).WithMessage("Số tầng hầm phải từ 1 đến 3.");
         }
     }
