50b57c3e5b1398e90ac63f0de1bde4c73e2f8ed6
Duy1408 <duynguyen08142001@gmail.com>
2024-12-02 09:33:54 +0700

    commit validation

---
+++ RealEstateProjectSale/Validations/Create/PropertyTypeCreateDTOValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using RealEstateProjectSaleBusinessObject.DTO.Create;
+
+namespace RealEstateProjectSale.Validations.Create
+{
+    public class PropertyTypeCreateDTOValidator : AbstractValidator<PropertyTypeCreateDTO>
+    {
+        public PropertyTypeCreateDTOValidator()
+        {
+            RuleFor(x => x.PropertyTypeName)
+             .NotEmpty().WithMessage("Loại căn hộ là bắt buộc.")
+             .MaximumLength(100).WithMessage("Loại căn hộ không được vượt quá 100 ký tự.");
+        }
+    }
+}
---
+++ RealEstateProjectSale/Validations/Update/PropertyTypeUpdateDTOValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using RealEstateProjectSaleBusinessObject.DTO.Create;
+using RealEstateProjectSaleBusinessObject.DTO.Update;
+
+namespace RealEstateProjectSale.Validations.Update
+{
+    public class PropertyTypeUpdateDTOValidator : AbstractValidator<PropertyTypeUpdateDTO>
+    {
+        public PropertyTypeUpdateDTOValidator()
+        {
+            RuleFor(x => x.PropertyTypeName)
+            .MaximumLength(100).WithMessage("Loại căn hộ không được vượt quá 100 ký tự.")
+            .When(x => !string.IsNullOrEmpty(x.PropertyTypeName));
+        }
+    }
+}
