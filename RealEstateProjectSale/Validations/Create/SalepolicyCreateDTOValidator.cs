696a819dcc0fcb6a011b2604d7598e2c9f158697
duongmake <duongmake3@gmail.com>
2024-12-02 10:17:07 +0700

    Fix Validate

--- RealEstateProjectSale/Validations/Create/SalepolicyCreateDTOValidator.cs
+++ RealEstateProjectSale/Validations/Create/SalepolicyCreateDTOValidator.cs
@@ -12,12 +12,13 @@ namespace RealEstateProjectSale.Validations.Create
               .MaximumLength(100).WithMessage("Loại chính sách bán hàng không được vượt quá 100 ký tự.");
 
             RuleFor(x => x.ExpressTime)
-              .NotEmpty().WithMessage("Ngày áo dụng là bắt buộc.")
+              .NotEmpty().WithMessage("Ngày áp dụng là bắt buộc.")
               .GreaterThanOrEqualTo(DateTime.Today).WithMessage("Ngày chọn phải từ hôm nay trở về sau.");
 
             RuleFor(x => x.PeopleApplied)
-              .MaximumLength(100).WithMessage("Loại chính sách bán hàng không được vượt quá 1000 ký tự.") 
-              .When(x => !string.IsNullOrEmpty(x.PeopleApplied)); 
+              .MaximumLength(100).WithMessage("Loại chính sách bán hàng không được vượt quá 1000 ký tự.")
+              .When(x => !string.IsNullOrEmpty(x.PeopleApplied));
+
             RuleFor(x => x.Status)
                 .Must(status => status == null || status == true || status == false).WithMessage("Trạng thái không hợp lệ nếu có.");
 
