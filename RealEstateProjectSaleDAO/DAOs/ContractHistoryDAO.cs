1c32f75b148539d5550ac686c0c9d6ca0d1ae916
Duy1408 <duynguyen08142001@gmail.com>
2024-12-16 10:21:47 +0700

    commit refund booking and dashboard

---
+++ RealEstateProjectSaleDAO/DAOs/ContractHistoryDAO.cs
@@ -0,0 +1,35 @@
+﻿using RealEstateProjectSaleBusinessObject.BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateProjectSaleDAO.DAOs
+{
+    public class ContractHistoryDAO
+    {
+
+        private static ContractHistoryDAO instance;
+
+        public static ContractHistoryDAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ContractHistoryDAO();
+                }
+                return instance;
+            }
+
+
+        }
+
+        //public List<ContractHistory> GetAllContractHistory()
+        //{
+        //    var _context = new RealEstateProjectSaleSystemDBContext();
+        //    return _context.Con.Include(c => c.Zone).ToList();
+        //}
+    }
+}
